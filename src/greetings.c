1342bdb085961c1b64cdeb51954984d47541b1f5
Marconia40 <mendrek@unc.edu.ar>
2024-03-18 10:51:18 -0300

    Setp 1 - CMake usage test and Commit with signature

---
+++ src/greetings.c
@@ -0,0 +1,10 @@
+#include "greetings.h"
+#include <stdio.h>
+
+/**
+ * @brief Marconia40
+ */
+void greetings()
+{
+    printf("Hello from another source file!\n");
+}
---
+++ src/main.c
@@ -0,0 +1,9 @@
+#include <greetings.h>
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+    printf("Hello from SOII, this is the project TP0\n");
+    greetings();
+    return 0;
+}
