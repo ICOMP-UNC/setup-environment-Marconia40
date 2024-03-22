7580166ee3b03304ab9ca2bc4ed9e41f50e6a577
Marconia40 <mendrek@unc.edu.ar>
2024-03-22 13:46:46 -0300

    Include Test in program

---
+++ include/logic.h
@@ -0,0 +1,37 @@
+#pragma once
+
+/**
+ * @brief Add two numbers
+ * 
+ * @param first_number
+ * @param second_number
+ * @return first_number + second_number
+*/
+int add(int first_number, int second_number);
+
+/**
+ * @brief Substract two numbers
+ * 
+ * @param first_number
+ * @param second_number
+ * @return first_number - second_number
+*/
+int substract(int first_number, int second_number);
+
+/**
+ * @brief Multiply two numbers
+ * 
+ * @param first_number
+ * @param second_number
+ * @return first_number * second_number
+*/
+int multiply(int first_number, int second_number);
+
+/**
+ * @brief Divide two numbers
+ * 
+ * @param first_number
+ * @param second_number
+ * @return first_number / second_number
+*/
+int divide(int first_number, int second_number);
