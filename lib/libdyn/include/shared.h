4b8753a1d4562ee9de2d55a569a38c1392601c38
Marconia40 <mendrek@unc.edu.ar>
2024-03-23 16:27:17 -0300

    Agregado de carpeta lib, por error en Pull Request

---
+++ lib/libdyn/include/shared.h
@@ -0,0 +1,11 @@
+#pragma once
+
+/**
+ * shared_lib_function
+ * 
+ * This function is defined in the shared library
+ * 
+ * @param msg: the message to be printed
+ * @return the message size.
+*/
+int shared_lib_function(char* msg);
